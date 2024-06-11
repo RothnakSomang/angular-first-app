9de63d6490fceb4bebe253fbbfc69dbac4e2d853
somangrothnak <41244899+RothnakSomang@users.noreply.github.com>
2024-06-11 23:36:22 +0700

    new project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component , inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form action="">
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location *ngFor="let housinglocation of housingLocationList" [housingLocation]="housinglocation"></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor(){
+    this.housingLocationList = this.housingService.getHousingLocationList();
+  }
+
+}
